89554e46cf4a162b99862ddc2a12d253aab0676c
antonella <antonella.gutierrezrodriguez@pcc.edu>
2020-01-09 16:21:51 -0800

    update

--- redirects-writer.js
+++ redirects-writer.js
@@ -1,7 +1,7 @@
 const { write } = require('./writer');
 
 const redirectsWriter = path => {
-  const redirects = `/*  /index.html   200`;
+  const redirects = '/*  /index.html   200';
 
   write(path + '/public/_redirects', redirects);
 };
